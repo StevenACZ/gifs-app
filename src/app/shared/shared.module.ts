694bbebfa77b91d58050bf68b03345141154f63a
steven <scoaila@proton.me>
2025-02-14 20:01:47 -0500

    add sidebar component and gifs module; update angular.json and index.html

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [SidebarComponent],
+  exports: [SidebarComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
