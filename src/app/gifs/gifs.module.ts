c061e6d07e56447cccd3375ee9e35c37a67e1450
steven <scoaila@proton.me>
2025-02-16 19:11:53 -0500

    add card list component and integrate it into the home page

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 // Components
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent],
+  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
   exports: [HomePageComponent],
   imports: [CommonModule],
 })
