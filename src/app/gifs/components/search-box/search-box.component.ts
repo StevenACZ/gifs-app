2395a8271433569927b7db2a869021e3c7987e8c
steven <scoaila@proton.me>
2025-02-17 19:18:11 -0500

    add search functionality to search box component

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-box',
   template: `
     <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Busca gifs" />
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Busca gifs"
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
   `,
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput') txtTagInput!: ElementRef<HTMLInputElement>;
+
   constructor() {}
+
+  searchTag() {
+    const newTag = this.txtTagInput.nativeElement.value;
+
+    console.log(newTag);
+  }
 }
