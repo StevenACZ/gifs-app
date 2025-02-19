e0eba22a52fc2a06bc684884a83d08a38a8e5b55
steven <scoaila@proton.me>
2025-02-19 17:57:20 -0500

    add HttpClientModule to app module and enhance GifsService with HTTP requests for tag search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 // Components
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GifsModule, SharedModule],
+  imports: [BrowserModule, HttpClientModule, GifsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+const GIPHY_API_KEY = 'aoC9XZRcgz9OMucMEF0Zajmli5sir3yZ';
+const SERVICE_URL = 'https://api.giphy.com/v1/gifs';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
   private _tagsHistory: string[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  // Functions
+  private organizeHistory(tag: string) {
+    tag = tag.trim().toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((olgTag) => olgTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+
+    this._tagsHistory = this._tagsHistory.slice(0, 10);
+  }
 
   searchTag(tag: string): void {
-    this._tagsHistory.unshift(tag);
+    if (tag.length === 0) return;
+
+    const params = new HttpParams()
+      .set('api_key', GIPHY_API_KEY)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http.get(`${SERVICE_URL}/trending`, { params }).subscribe((res) => {
+      console.log(res);
+    });
+
+    this.organizeHistory(tag);
   }
 
   // GETTERS
